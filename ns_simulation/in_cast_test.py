34777e3e28f7fd8bc367d6c871f5b9f9be82a7dc
ketchup98BUPT <32302166+ketchup98BUPT@users.noreply.github.com>
2024-10-18 16:07:44 +0800

    Update in_cast_test.py

--- ns_simulation/in_cast_test.py
+++ ns_simulation/in_cast_test.py
@@ -11,6 +11,7 @@ from ns.switch.switch import SimplePacketSwitch
 
 def packet_arrival():
     """Packets arrive with a constant interval of 0.1 seconds."""
+    # 10G
     return 0.1
 
 
@@ -21,34 +22,48 @@ def packet_size():
 
 def delay_dist():
     """Network wires experience a constant propagation delay of 0.1 seconds."""
+    # 10m
     return 0.1
 
 
 # 1 * receiver, in_cast_degree * sender, 1 * switch
-in_cast_degree = 2
+in_cast_degree = 8
+port_rate_bps = 16384  # bps
+# buffer_size=1000 # in packets
+flow_size = 100000  # in bytes
 
 env = simpy.Environment()
 
 switch = SimplePacketSwitch(
     env,
     nports=in_cast_degree + 1,
-    port_rate=16384,  # in bits/second
-    buffer_size=5,  # in packets
+    port_rate=port_rate_bps,  # in bits/second
+    buffer_size=1000,  # in packets
     debug=True,
 )
 # switch 端口连接：0端口接“汇聚节点”，N端口接sender N ...
 # Sender id 从1开始
 
-receiver = TCPSink(env, rec_waits=True, debug=True)
+# agg_node 端口连接：0端口接switch，N端口接sink N ...
+agg_node = SimplePacketSwitch(
+    env,
+    nports=in_cast_degree + 1,
+    port_rate=port_rate_bps,  # in bits/second
+    buffer_size=1000,  # in packets
+    debug=True,
+)
 
 flow_bank = []
 sender_bank = []
+# fib for switch
 fib = {}
+# fib for agg.node
+fib_an = {}
 
 for sender_id in range(1, in_cast_degree+1):
     # New object
-    new_flow = Flow(fid=sender_id, src="flow " + str(sender_id), dst="flow " + str(sender_id), finish_time=10,
-                    arrival_dist=packet_arrival, size_dist=packet_size, )
+    new_flow = Flow(fid=sender_id, src="flow " + str(sender_id), dst="flow " + str(sender_id), finish_time=100000,
+                    arrival_dist=packet_arrival, size_dist=packet_size, size=flow_size)
     new_sender = TCPPacketGenerator(env, flow=new_flow, cc=TCPReno(), element_id=new_flow.src, debug=True)
     new_wire1_downstream = Wire(env, delay_dist)
     new_wire1_upstream = Wire(env, delay_dist)
@@ -66,11 +81,28 @@ for sender_id in range(1, in_cast_degree+1):
     switch.demux.outs[sender_id].out = new_wire1_upstream
     new_wire1_upstream.out = new_sender
 
+    fib_an[sender_id] = sender_id
+    fib_an[sender_id + 10000] = 0
+    new_receiver = TCPSink(env, rec_waits=True, debug=True)
+    agg_node.demux.outs[sender_id].out = new_receiver
+    new_receiver.out = agg_node
+
+
+# connecting object: on the side of receiver
+wire2_downstream = Wire(env, delay_dist)
+wire2_upstream = Wire(env, delay_dist)
 
 switch.demux.fib = fib
-# receiver
-switch.demux.outs[0] = receiver
-receiver.out = switch
+switch.demux.outs[0].out = wire2_downstream
+wire2_downstream.out = agg_node
 
-env.run(until=1)
+agg_node.demux.fib = fib_an
+agg_node.demux.outs[0].out = wire2_upstream
+wire2_upstream.out = switch
+
+
+env.run(until=1000)
+
+
+
 
